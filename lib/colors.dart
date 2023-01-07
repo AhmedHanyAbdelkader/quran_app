d2ffe9cb99f876c78835bddcb98e25edcdc7034b
AhmedHanyAbdelkader <ah2617557@gmail.com>
2023-01-07 18:20:51 +0200

    commit app light colors

---
+++ lib/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppLightColors{
+  static const Color orange = Color(0xffEA7131);
+  static const Color deepOrange = Color(0xffED3727);
+  static const Color gray = Color(0xff3A3E45);
+  static const Color black = Color(0xff292C31);
+
+}
