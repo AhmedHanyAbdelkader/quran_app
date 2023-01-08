f7be8bb7d15ab920a8d762829b24767583065c80
eyadmohamedelzeky <eyadmohamedelzeky@gmail.com>
2023-01-08 19:08:53 +0200

    add package

--- lib/layouts/splash_screen.dart
+++ lib/layouts/splash_screen.dart
@@ -1,4 +1,6 @@
+import 'package:easy_splash_screen/easy_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:quran_app/layouts/home_screen.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -7,8 +9,14 @@ class SplashScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Image.asset('assets/images/quraan.png'),
+      body: EasySplashScreen(
+        logo: Image.asset('assets/images/quraan.png'),
+        navigator: HomeScreen(),
+        loaderColor: Colors.red,
+        loadingText: Text(
+          'Loading......',
+          style: TextStyle(color: Colors.orange),
+        ),
       ),
     );
   }
